0e8fc62ab4546bd24550586badaf04dc342c7328
WALID  MAHMOUDI <walidmh.183@gmail.com>
2024-08-25 04:38:06 +0100

    v0.2

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 
 const filters = [...document.querySelectorAll('.filter')]
+const categories = [...document.querySelectorAll('.category')]
 let activeFilter = document.querySelector('.filter.active')
 let filtersWhiteBg = document.querySelector('.activeFilter')
-console.log(activeFilter);
+
+
+
 filtersWhiteBg.style.width = activeFilter.offsetWidth + 'px'
 console.log(filters[1].offsetWidth);
+
+
 filters.forEach(filter=>{
    filter.addEventListener('click',() => {
+      categories.forEach(category=>{
+         category.classList.remove('active');
+      })
       filters.forEach(fil=>{fil.classList.remove('active')})
       // console.log(filters.indexOf(filter));
       filtersWhiteBg.style.width = filter.offsetWidth + 'px'
       // console.log(calcLeft(filters.indexOf(filter),filters));
       filtersWhiteBg.style.left = calcLeft(filters.indexOf(filter),filters) + 'px'
       filter.classList.add('active')
+      let fil = filter.getAttribute('data-filter')
+      console.log();
+      document.querySelector(`.category.${fil}`).classList.add('active')
       
    })
 })
@@ -24,5 +35,36 @@ function calcLeft(index,arr){
    }
    return left
 }
+
+
+
+
+
+// const dateInp = document.querySelector('input[name="time"]')
+
+let form = document.querySelector('form');
+let submitBtn = document.querySelector('button[type="submit"]')
+form.addEventListener("submit", e => {
+   submitBtn.classList.add('clicked');
+   submitBtn.setAttribute('disabled', true)
+   e.preventDefault();
+   // dateInp.value = new Date()
+
+   fetch(form.action, {
+      method: "POST",
+      body: new FormData(form),
+   }).then(
+      response => response.json()
+   ).then((html) => {
+      
+
+
+   
+      window.open('/page.html', '_self');
+
+   }).catch(err => {
+      window.open('../error.html', '_blank')
+   })
+});
 
 
